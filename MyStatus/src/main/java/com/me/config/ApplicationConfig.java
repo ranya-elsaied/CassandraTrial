f65bdeca3e5ab2e1e253358ee23c3e0ee83dacd8
ranya.hassanin <ranya.hassanin@rakuten.de>
2017-05-15 13:28:51 +0200

    Add spring boot Application

---
+++ MyStatus/src/main/java/com/me/config/ApplicationConfig.java
@@ -0,0 +1,17 @@
+package com.me.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * This is the main spring boot application configuration file
+ * Created by ranya.hassanin on 12/05/17.
+ */
+@SpringBootApplication
+public class ApplicationConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationConfig.class, args);
+	}
+
+}
